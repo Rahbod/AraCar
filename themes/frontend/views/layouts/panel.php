da778c3b86268260b10bbf1840666f1fcec3e01f
Yusef Mobasheri <yusef.mobasheri@gmail.com>
2017-12-21 12:47:36 +0330

    Fix Bugs

--- themes/frontend/views/layouts/panel.php
+++ themes/frontend/views/layouts/panel.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <meta name="csrf-token" content="<?= Yii::app()->request->csrfToken ?>" />
     <meta name="keywords" content="<?= $this->keywords ?>">
     <meta name="description" content="<?= $this->description?> ">
     <title><?= (!empty($this->pageTitle)?$this->pageTitle.' | ':'').$this->siteName ?></title>
