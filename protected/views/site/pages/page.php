479b3ce7acbecb4b4c200fe305277e6e8f7185ff
Yusef Mobasheri <yusef.mobasheri@gmail.com>
2017-12-24 18:35:01 +0330

    Fix Bugs

--- protected/views/site/pages/page.php
+++ protected/views/site/pages/page.php
@@ -1,12 +1,24 @@
 <?php
 /* @var $this SiteController */
-/* @var $error array */
-
-$this->pageTitle= Yii::app()->name . ' - '.$model->title;
+/* @var $model Pages */
+$this->pageTitle=$model->title;
+$this->pageHeader=$model->title;
+$this->breadcrumbs=array(
+    $model->title,
+);
 ?>
-<div class="page">
-    <div class="container page-content">
-        <h1><?php echo $model->title; ?></h1>
-        <div class="text-content"><?= $model->summary; ?></div>
+
+<?php $this->renderPartial('//partial-views/_breadcrumb') ?>
+<div class="content-box white-bg page-view">
+    <div class="center-box relative">
+        <div class="row">
+            <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12">
+                <div class="page-text" dir="auto"><?php
+                    $purifier=new CHtmlPurifier();
+                    echo $purifier->purify($model->summary);
+                    ?></div>
+            </div>
+            <div class="fade-logo-bg"></div>
+        </div>
     </div>
 </div>
