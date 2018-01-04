3d05376f7361c2a566c7a20567b972ed39ed58bd
Yusef Mobasheri <yusef.mobasheri@gmail.com>
2018-01-04 18:12:12 +0330

    change filters

--- protected/modules/car/views/search/dealership.php
+++ protected/modules/car/views/search/dealership.php
@@ -4,13 +4,37 @@
 /* @var $filters [] */
 /* @var $form CActiveForm */
 /* @var $dataProvider CActiveDataProvider */
-
-$this->breadcrumbs = array(
-	'نمایشگاه ها' => array('/dealerships'),
-	$model->dealership_name
-);
 ?>
 
+<div class="page-header">
+	<div class="top">
+		<div class="center-box">
+			<div class="row">
+				<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12"></div>
+				<div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+					<?php if($this->pageLogo):?><img src="<?= $this->pageLogo ?>" class="brand-logo" alt="<?= strip_tags($this->pageHeader) ?>"><?php endif; ?>
+					<h2 class="brand-name<?= $this->layout == '//layouts/panel'?' user-detail':'' ?>"><?php echo $this->pageHeader ?><small><?php echo $this->pageDescription ?></small></h2>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php if(count($filters)):?>
+		<div class="bottom">
+			<div class="center-box">
+				<div class="row">
+					<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12"></div>
+					<div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+						<span>انتخاب شما</span>
+						<div class="filters">
+							<?= $this->createFiltersBar($filters);?>
+							<a href="<?= $this->createUrl('/'.Yii::app()->request->pathInfo)?>" class="clear-filters-link">پاک کردن همه</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php endif;?>
+</div>
 <div class="content-box">
 	<div class="center-box">
 		<div class="row">
