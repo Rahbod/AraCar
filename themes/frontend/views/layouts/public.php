e73964b83ca82ad292e1049bb491f444cccc0928
Yusef Mobasheri <yusef.mobasheri@gmail.com>
2018-01-10 21:08:14 +0330

    Responsive Finished

--- themes/frontend/views/layouts/public.php
+++ themes/frontend/views/layouts/public.php
@@ -27,12 +27,12 @@
     $cs->registerCssFile($baseUrl.'/css/bootstrap-rtl.min.css');
     $cs->registerCssFile($baseUrl.'/css/fontiran.css');
     $cs->registerCssFile($baseUrl.'/css/font-awesome.css');
-    $cs->registerCssFile($baseUrl.'/css/bootstrap-theme.css?3.1');
-    $cs->registerCssFile($baseUrl.'/css/responsive-theme.css?3.1');
+    $cs->registerCssFile($baseUrl.'/css/bootstrap-theme.css?3.3');
+    $cs->registerCssFile($baseUrl.'/css/responsive-theme.css?3.3');
 
     $cs->registerScriptFile($baseUrl.'/js/bootstrap.min.js', CClientScript::POS_END);
     $cs->registerScriptFile($baseUrl.'/js/jquery.nicescroll.min.js', CClientScript::POS_END);
-    $cs->registerScriptFile($baseUrl.'/js/jquery.script.js?3.1', CClientScript::POS_END);
+    $cs->registerScriptFile($baseUrl.'/js/jquery.script.js?3.3', CClientScript::POS_END);
     ?>
 </head>
 <body>
