c3c69b947e26246efaea59c1b05671f662dfc19e
y_mobasheri <yusef.mobasheri@gmail.com>
2019-02-13 13:37:49 +0330

    Fix bugs

--- protected/modules/advertises/views/manage/_advertise_item.php
+++ protected/modules/advertises/views/manage/_advertise_item.php
@@ -2,17 +2,19 @@
 /** @var $placement string */
 /** @var $cssClass string */
 /** @var $dismissible boolean */
+/** @var $ad Advertises */
 
 $dismissible = isset($dismissible)?$dismissible:false;
-
-if($ad = Advertises::GetInPlacement($placement)):
-    if($ad): if($ad->type == Advertises::TYPE_BANNER && $ad->banner && is_file(Yii::getPathOfAlias('webroot')."/uploads/advertises/$ad->banner")):?>
-            <div class="advertise <?= $cssClass ?>">
-                <?php if($dismissible):?><strong class="advertise-close"></strong><?php endif; ?>
-                <a href="<?= $ad->link ?>" rel="nofollow" target="_blank">
-                    <div class="advertise-image" style="background-image: url('<?= Yii::app()->getBaseUrl(true)."/uploads/advertises/$ad->banner" ?>');"></div>
-                </a>
-            </div>
-        <?php elseif($ad->type == Advertises::TYPE_SCRIPT && !empty($ad->script)):?>
-            <div class="advertise advertise-script <?= $cssClass ?>"><?= $ad->script ?></div>
-<?php endif;endif;endif; ?>
+$ad = Advertises::GetInPlacement($placement);
+if($ad):
+    if($ad->type == Advertises::TYPE_BANNER && $ad->banner && is_file(Yii::getPathOfAlias('webroot')."/uploads/advertises/$ad->banner")):?>
+        <div class="advertise <?= $cssClass ?>">
+            <?php if($dismissible):?><strong class="advertise-close"></strong><?php endif; ?>
+            <a href="<?= $ad->link ?>" rel="nofollow" target="_blank">
+                <div class="advertise-image" style="background-image: url('<?= Yii::app()->getBaseUrl(true)."/uploads/advertises/$ad->banner" ?>');"></div>
+            </a>
+        </div>
+    <?php elseif($ad->type == Advertises::TYPE_SCRIPT && !empty($ad->script)):?>
+        <div class="advertise advertise-script <?= $cssClass ?>"><?= $ad->script ?></div>
+<?php endif;
+endif; ?>
